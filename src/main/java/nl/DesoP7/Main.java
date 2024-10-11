b38573cbe2c636deed35c49255cef22e1716e5cf
Shamir Martha <divinegamers@hotmail.com>
2024-10-11 10:31:50 +0200

    case 1 tot 9 extract method (ctrl + shift + m) om alles wat netter te krijgen

--- src/main/java/nl/DesoP7/Main.java
+++ src/main/java/nl/DesoP7/Main.java
@@ -32,114 +32,159 @@ public class Main {
             // Switch case to handle user input
             switch (choice) {
                 case 1:
-                    // Add a Book
-                    System.out.print("Enter Book ID: ");
-                    long id = scanner.nextLong();
-                    scanner.nextLine(); // consume newline
-
-                    System.out.print("Enter Book Title: ");
-                    String title = scanner.nextLine();
-
-                    System.out.print("Enter Book Author: ");
-                    String author = scanner.nextLine();
-
-                    System.out.print("Enter Number of Pages: ");
-                    int pages = scanner.nextInt();
-                    scanner.nextLine(); // consume newline
-
-                    System.out.print("Enter Book ISBN: ");
-                    String isbn = scanner.nextLine();
-
-                    library.addBook(id, title, author, pages, isbn);
-                    System.out.println("Book added successfully.\n");
+                    // Add a book
+                    addBook(scanner, library);
                     break;
 
                 case 2:
                     // Remove a Book
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books in the library to remove.\n");
-                    } else {
-                        System.out.print("Enter Book ID to remove: ");
-                        long removeId = scanner.nextLong();
-                        library.removeBook(removeId);
-                        System.out.println("Book removed successfully.\n");
-                    }
+                    removeBook(library, scanner);
                     break;
 
                 case 3:
                     // Show Book Details
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books in the library.\n");
-                    } else {
-                        System.out.print("Enter Book ID to show details: ");
-                        long showId = scanner.nextLong();
-                        System.out.println(library.showBook(showId));
-                    }
+                    showDetails(library, scanner);
                     break;
 
                 case 4:
                     // Show All Books
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books in the library.\n");
-                    } else {
-                        System.out.println("All Books:");
-                        System.out.println(library.showBooks());
-                    }
+                    allBooks(library);
                     break;
 
                 case 5:
                     // Borrow a Book
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books in the library to borrow.\n");
-                    } else {
-                        System.out.print("Enter Book ID to borrow: ");
-                        long borrowId = scanner.nextLong();
-                        library.borrowBook(borrowId);
-                        System.out.println("Book borrowed successfully.\n");
-                    }
+                    borrowBook(library, scanner);
                     break;
 
                 case 6:
                     // Return a Book
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books to return.\n");
-                    } else {
-                        System.out.print("Enter Book ID to return: ");
-                        long returnId = scanner.nextLong();
-                        library.returnBook(returnId);
-                        System.out.println("Book returned successfully.\n");
-                    }
+                    returnBook(library, scanner);
                     break;
 
                 case 7:
                     // Show Borrowed Books
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books that have been borrowed.\n");
-                    } else {
-                        System.out.println("Borrowed Books:");
-                        System.out.println(library.showBorrowedBooks());
-                    }
+                    showBorrowedBooks(library);
                     break;
 
                 case 8:
                     // Show Available Books
-                    if (library.isLibraryEmpty()) {
-                        System.out.println("There are no books available in the library.\n");
-                    } else {
-                        System.out.println("Available Books:");
-                        System.out.println(library.showAvailableBooks());
-                    }
+                    showAvailableBooks(library);
                     break;
 
                 case 9:
-                    // Exit the program
-                    System.out.println("Exiting Library System. Goodbye!");
-                    scanner.close();
+                    // Exit the Program
+                    Exit(scanner);
                     return;
 
                 default:
                     System.out.println("Invalid choice. Please try again.\n");
             }
         }
+    }
+
+    private static void Exit(Scanner scanner) {
+        // Exit the program
+        System.out.println("Exiting Library System. Goodbye!");
+        scanner.close();
+    }
+
+    private static void showAvailableBooks(Library library) {
+        // Show Available Books
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books available in the library.\n");
+        } else {
+            System.out.println("Available Books:");
+            System.out.println(library.showAvailableBooks());
+        }
+    }
+
+    private static void showBorrowedBooks(Library library) {
+        // Show Borrowed Books
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books that have been borrowed.\n");
+        } else {
+            System.out.println("Borrowed Books:");
+            System.out.println(library.showBorrowedBooks());
+        }
+    }
+
+    private static void returnBook(Library library, Scanner scanner) {
+        // Return a Book
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books to return.\n");
+        } else {
+            System.out.print("Enter Book ID to return: ");
+            long returnId = scanner.nextLong();
+            library.returnBook(returnId);
+            System.out.println("Book returned successfully.\n");
+        }
+    }
+
+    private static void borrowBook(Library library, Scanner scanner) {
+        // Borrow a Book
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books in the library to borrow.\n");
+        } else {
+            System.out.print("Enter Book ID to borrow: ");
+            long borrowId = scanner.nextLong();
+            library.borrowBook(borrowId);
+            System.out.println("Book borrowed successfully.\n");
+        }
+    }
+
+    private static void allBooks(Library library) {
+        // Show All Books
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books in the library.\n");
+        } else {
+            System.out.println("All Books:");
+            System.out.println(library.showBooks());
+        }
+    }
+
+    private static void showDetails(Library library, Scanner scanner) {
+        // Show Book Details
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books in the library.\n");
+        } else {
+            System.out.print("Enter Book ID to show details: ");
+            long showId = scanner.nextLong();
+            System.out.println(library.showBook(showId));
+        }
+    }
+
+    private static void removeBook(Library library, Scanner scanner) {
+        // Remove a Book
+        if (library.isLibraryEmpty()) {
+            System.out.println("There are no books in the library to remove.\n");
+        } else {
+            System.out.print("Enter Book ID to remove: ");
+            long removeId = scanner.nextLong();
+            library.removeBook(removeId);
+            System.out.println("Book removed successfully.\n");
+        }
+    }
+
+    private static void addBook(Scanner scanner, Library library) {
+        // Add a Book
+        System.out.print("Enter Book ID: ");
+        long id = scanner.nextLong();
+        scanner.nextLine(); // consume newline
+
+        System.out.print("Enter Book Title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter Book Author: ");
+        String author = scanner.nextLine();
+
+        System.out.print("Enter Number of Pages: ");
+        int pages = scanner.nextInt();
+        scanner.nextLine(); // consume newline
+
+        System.out.print("Enter Book ISBN: ");
+        String isbn = scanner.nextLine();
+
+        library.addBook(id, title, author, pages, isbn);
+        System.out.println("Book added successfully.\n");
     }
 }
